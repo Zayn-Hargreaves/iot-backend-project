6e4d9890e75d78394bbc35fd072b8d3bed84afcc
Quân vũ <quanva.b21cn618@stu.ptit.edu.vn>
2024-11-09 11:18:42 +0700

    test

--- src/socket.io.js
+++ src/socket.io.js
@@ -4,6 +4,11 @@ const SocketIO = require("socket.io");
 const app = require("./app");  // import app từ `app.js`
 
 const server = http.createServer(app);
-const io = SocketIO(server);
+const io = socketIO(server, {
+    cors: {
+      origin: "*",  // Cho phép tất cả các nguồn kết nối đến server
+      methods: ["GET", "POST"]  // Các phương thức HTTP được phép
+    }
+});
 
 module.exports = { io, server };
