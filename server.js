9eade9a607b7013202371d0d50b2c39e53e099eb
Quân vũ <quanva.b21cn618@stu.ptit.edu.vn>
2024-11-09 10:07:06 +0700

    Fix socket.io

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./src/app")
+const {app} = require("./src/app")
 const  PORT = process.env.PORT || 3000
 const server = app.listen(PORT, ()=>{
     console.log(`server is listening on port ${PORT}`);
